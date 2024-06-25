6f9e8d37a87e34e6a1d87c0100fbaae510f628fa
aditir360 <71522856+aditir360@users.noreply.github.com>
2024-06-24 22:24:40 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+var myGame = new WizardOrpheus('', `You're an investor who has many startup business founders come to you. They want you to invest in their company. However, you need to invest wisely and not be persuaded too easily.
+`)
+
+myGame.createUserAction({
+  
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+  
+})
+
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    
+    myGame.message(userInput)
+
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
+
+    document.getElementById('input').value = ''
+  }
+})
+
+myGame.createUserAction({
+  
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+  
+})
+
+
+myGame.variable('score', 'Current score. Changes (positive and negatively) as the user does things.', 0)
+
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+ 
+document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+
+document.getElementById('score').innerHTML = data.currentVariables.score.value
+  
+})
+
